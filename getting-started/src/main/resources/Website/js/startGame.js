1134418508c93e08eba0bead7200d9dc82d8f08b
GanausRichard <richard.ganaus@gmail.com>
2021-12-28 20:29:55 +0100

    startGame works now

--- getting-started/src/main/resources/Website/js/startGame.js
+++ getting-started/src/main/resources/Website/js/startGame.js
@@ -1,25 +1,31 @@
-function getGameMode(buttonId) {
-    const btnId = buttonId;
+let gameMode = "pve";
+
+function getGameMode(value) {
+    gameMode = value;
 }
 
-const form = document.querySelector("#gameSettings");
-
-form.addEventListener("submit", function (event) {
-    // stop form submission
-    event.preventDefault();
-
-    // if valid, submit the form
+window.onload = function startGame() {
     const form = document.getElementById("gameSettings");
-    const name = form.elements["name"].value;
-    const gameMode = form.elements["gameMode1"].value;
-    const data = {name, gameMode};
 
-    const myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
+    form.addEventListener("submit", function (event) {
+        // stop form submission
+        event.preventDefault();
 
-    fetch('http://localhost:8080/connectFour/start', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: myHeaders})
-        .then(response => {console.log("game has started", response.json()); });
-});
+        // if valid, submit the form
+        const name = form.elements["name"].value;
+        const data = {name, gameMode};
+
+        const myHeaders = new Headers();
+        myHeaders.append('Content-Type', 'application/json');
+
+        fetch('http://localhost:8080/connectFour/start', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: myHeaders})
+            .then(response => {
+                console.log("game has started", response.json());
+                window.location.href = "/staticFiles/html/doTurn";
+            });
+    });
+}
+
