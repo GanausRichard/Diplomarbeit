22f016c48c80d7568f6d147872528ddb36d7dd89
GanausRichard <richard.ganaus@gmail.com>
2022-03-17 23:04:24 +0100

    Revert "added comments to some classes and changed pom file to include EasyModbus Library"
    This reverts commit 63216233

--- getting-started/src/main/java/org/acme/model/ConnectFourException.java
+++ getting-started/src/main/java/org/acme/model/ConnectFourException.java
@@ -3,7 +3,6 @@ package org.acme.model;
 public class ConnectFourException extends Exception {
     public String message;
 
-    //class constructor: an exception message has to be passed to create a new connect four exception
     public ConnectFourException(String message) {
         this.message = message;
     }
